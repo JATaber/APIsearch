f816def2817b002d87960de6efa49ea0b68db616
James Taber <JATaber@fullsail.edu>
2017-06-02 00:44:28 -0500

    more setting up file structor

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ gulp.task('default', function(){
 });
 
 gulp.task('scss', function(){
-   return gulp.src('./scss/**/*.scss')
+   return gulp.src('./scss/*.scss')
        .pipe(sass())
        .pipe(gulp.dest('./css'))
        .pipe(browserSync.reload({
