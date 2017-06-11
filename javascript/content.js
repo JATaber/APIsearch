0fc1d999a705197eebc235837ebb0dbcd9fa1c8d
James Taber <JATaber@fullsail.edu>
2017-06-10 21:18:39 -0500

    working on dropdown

--- javascript/content.js
+++ javascript/content.js
@@ -56,4 +56,11 @@ request.open('GET', url, true);
 request.send();
 
 
-
+function show(){
+    var x = document.getElementById('nav');
+    if(x.style.display === 'none'){
+        x.style.display = 'block';
+    }else{
+        x.style.display = 'none';
+    }
+}
